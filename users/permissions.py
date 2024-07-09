8161a781124e6559342aeaefe5187b20aa8dbb26
moSa963 <mo.sa963@hotmail.com>
2024-07-09 21:24:30 +0300

    Remove permissions file

--- users/permissions.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from follow.permissions import is_user_following
-
-def is_user_accessble(request, user:User) ->bool:
-    
-    if user.id != request.user.id and user.is_private:
-        if not request.user.is_authenticated or not is_user_following(request.user, user):
-            return False
-    return True
