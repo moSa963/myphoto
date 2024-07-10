a0e7dd516273de17a2dc298fb8c33d720954a430
moSa963 <mo.sa963@hotmail.com>
2024-07-10 20:47:48 +0300

    Refactor views and urls to improve readability and maintainability

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path
-from django.urls.conf import include
-from .views import Register, Login, UserImageView, UserList, UserView, ProfileView
+from .views import Register, User, Login, AuthUser, UserImageView, UsersList
 from rest_framework_simplejwt.views import TokenRefreshView
 
+app_name = "users"
+
 urlpatterns = [
-    path('register', Register.as_view()),
-    path('login', Login.as_view()),
+    path('', AuthUser.as_view(), name="user"),
+    path('login/', Login.as_view(), name="login"),
     path('token/refresh', TokenRefreshView.as_view()),
-    path('user', ProfileView.as_view()),
-    path('user/<str:username>', UserView.as_view()),
-    path('user/image/<str:username>', UserImageView.as_view()),
-    path('user/list/<str:key>', UserList.as_view()),
-    path('post/', include('post.urls')),
-    path('follow/', include('follow.urls')),
-    path('history/', include('history.urls')),
-    path('comment/', include('comment.urls')),
-    path('like/', include('like.urls')),
+    path('register/', Register.as_view(), name="register"),
+    path('list', UsersList.as_view()),
+    path('<str:username>', User.as_view()),
+    path('<str:username>/image', UserImageView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,125 +1,83 @@
-from functools import partial
-from django.db.models.expressions import F, Case, When
-from django.db.models.fields import BooleanField
-from django.db.models.query_utils import Q
-from django.http.response import FileResponse
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView, ListAPIView
-from rest_framework import status
-from .serializers import LoginSerializer, RegisterSerializer, UserUpdate
-from rest_framework_simplejwt import tokens
-from django.db.models.aggregates import Count
-from .models import UserProfile
+from .serializers import UserCreateSerializer, UserSerializer, LoginSerializer, UserUpdateSerializer
+from rest_framework import permissions
 from rest_framework.response import Response
-from .serializers import UserSerializer
-from api import models
-from rest_framework.permissions import IsAuthenticated
-from .permissions import is_user_accessble
-# Create your views here.
+from rest_framework_simplejwt import tokens
+from .models import User as UserModel
+from django.shortcuts import get_object_or_404
+from django.http.response import FileResponse
+from rest_framework import status 
+
+class AuthUser(GenericAPIView):
+    serializer_class=UserSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(instance=request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        serializer = UserUpdateSerializer(instance=request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=UserSerializer(instance=request.user).data)
 
 class Register(GenericAPIView):
-    serializer_class = RegisterSerializer
+    serializer_class=UserCreateSerializer
+    permission_classes=[permissions.AllowAny]
 
     def post(self, request):
-        ser = self.get_serializer(data=request.data)
-        ser.is_valid(raise_exception=True)
-        user = ser.save()
+        serializer = UserCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
 
         refresh_token = tokens.RefreshToken.for_user(user)
 
         return Response({
             'user': UserSerializer(user).data,
             'refresh': str(refresh_token),
-            'access': str(refresh_token.access_token)}, status=status.HTTP_200_OK)
-
+            'access': str(refresh_token.access_token)}, status=status.HTTP_201_CREATED)
 
 class Login(GenericAPIView):
-    serializer_class = LoginSerializer
+    serializer_class=LoginSerializer
+    permission_classes=[permissions.AllowAny]
 
-    #get the authenticated user
-    def get(self, request):
-        if request.user.is_authenticated:
-            user = UserProfile.objects.annotate(
-                followers_count=Count("followers", filter=Q(followers__is_verified=True)), followed_count=Count("following", filter=Q(following__is_verified=True))
-                ).get(id=request.user.id)
-
-            return Response(data=UserSerializer(user).data, status=status.HTTP_200_OK)
-
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-    #login
     def post(self, request):
-        ser = LoginSerializer(data=request.data)
-        ser.is_valid()
-        try:
-            user = ser.save()
-            refresh_token = tokens.RefreshToken.for_user(user)
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
 
-            user = UserProfile.objects.annotate(
-                followers_count=Count("followers", filter=Q(followers__is_verified=True)), followed_count=Count("following", filter=Q(following__is_verified=True))
-                ).get(id=user.id)
+        refresh_token = tokens.RefreshToken.for_user(user)
 
-            return Response({'user': UserSerializer(user).data,
-             'refresh': str(refresh_token),
-             'access': str(refresh_token.access_token)}, status=status.HTTP_200_OK)
-        except:
-            return Response({"username": ['username or password are wrong',],}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserImageView(GenericAPIView):
-
-    def get(self, requset, **kwargs):
-        username = kwargs.get('username', 0)
-        try:
-            user = UserProfile.objects.get(username=username)
-            return FileResponse(open(str(user.image), 'rb'))
-        except:
-            return FileResponse(open('media/defaults/user.png', 'rb'))
-
-
-class UserList(ListAPIView):
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        key = self.kwargs.get('key', 0)
-        user_id = self.request.user.id
-
-        return UserProfile.objects.filter(username__icontains=key
-            ).exclude(id=user_id).annotate(followers_count=Count("followers"), followed_count=Count("following"),
-            is_following=Case(
-                When(followers__user__id=user_id, then=F("followers__is_verified")),
-                output_field=BooleanField(null=True)
-            )
-        )
-
-
-class UserView(GenericAPIView):
-    serializer_class = UserSerializer
+        return Response({
+            'user': UserSerializer(user).data,
+            'refresh': str(refresh_token),
+            'access': str(refresh_token.access_token)})
+    
+class User(GenericAPIView):
+    serializer_class=UserSerializer
+    permission_classes=[permissions.IsAuthenticated]
 
     def get(self, request, **kwargs):
-        username = kwargs.get("username", "")
-        user_id = getattr(request.user, "id", 0)
+        user = get_object_or_404(UserModel, username=kwargs['username'])
+        serializer = UserSerializer(instance=user)
+        return Response(serializer.data)
 
-        user = UserProfile.objects.filter(username=username
-        ).annotate(followers_count=Count("followers", filter=Q(followers__is_verified=True)),
-         following_count=Count("following", filter=Q(following__is_verified=True)),
-         is_following=Case(
-             When(followers__user__id=user_id, then=F("followers__is_verified")),
-             output_field=BooleanField(null=True)
-         )).first()
+class UserImageView(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-        userSer = self.get_serializer(user)
+    def get(self, request, **kwargs):
+        user = get_object_or_404(UserModel, username=kwargs['username'])
+        return FileResponse(open(user.image.path, 'rb'))
 
-        return Response(data=userSer.data, status=status.HTTP_200_OK)
+class UsersList(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        key = self.request.GET.get('key', '')
 
+        query = UserModel.objects.filter(username__icontains=key)
 
-class ProfileView(GenericAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = UserUpdate
-
-    def put(self, request):
-        serializer = self.get_serializer(request.user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return query
