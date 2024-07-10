0e3d73486c66cc486155f325dcc114b9a9885caa
moSa963 <mo.sa963@hotmail.com>
2024-07-10 20:43:48 +0300

    Refactor user model and serializers

--- users/models.py
+++ users/models.py
@@ -1,26 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import os;
-import glob;
-from myphoto import settings
-
-# Create your models here.
+import os
+from uuid import uuid4
 
 def image_name(instance, filename):
-    rest, suf = filename.rsplit('.', 1)
-    username = instance.username or instance.user.username
-    new_name = 'media/img/user/' + username + "." + suf
-
-    #if there is a file with the same name delete it.
-    for filepath in glob.glob(str(settings.BASE_DIR) + '/media/img/user/' + str(username) + '*'):
-        name, end = os.path.basename(filepath).rsplit('.', 1)
-        if name == username:
-            os.remove(filepath)
-
-    return new_name
+    ext = filename.split('.')[-1]
+    return f'profile_image/{uuid4()}.{ext}'
 
 
-class UserProfile(AbstractUser):
-    is_private = models.BooleanField(default=False)
-    is_verified = models.BooleanField(default=False)
+# Create your models here.
+class User(AbstractUser):
+    private = models.BooleanField(default=False)
+    verified = models.BooleanField(default=False)
     image = models.ImageField(upload_to=image_name, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_image = User.objects.get(pk=self.pk).image
+            if self.image != old_image and os.path.isfile(old_image.path):
+                os.remove(old_image.path)
+        super().save(*args, **kwargs)
+    
--- users/serializers.py
+++ users/serializers.py
@@ -1,88 +1,79 @@
-from os import read, write
-from django.db.models import fields
 from rest_framework import serializers
-from .models import UserProfile
+from .models import User
 from rest_framework import authentication
 
-from api import models
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(min_length=3, max_length=30)
-    last_name =  serializers.CharField(min_length=3, max_length=30)
-    username =   serializers.RegexField(regex=r'^[a-zA-Z]+([._-]?[a-zA-Z0-9]+)+$', min_length=3, max_length=30)
-    email =      serializers.EmailField(min_length=5, write_only=True)
-    password =   serializers.CharField(min_length=8, max_length=50, write_only=True)
-    is_private =   serializers.BooleanField(read_only=True, default=False)
-    is_verified =  serializers.BooleanField(read_only=True, default=False)
-    image = serializers.ImageField(required=False, write_only=True)
-    followers_count = serializers.IntegerField(read_only=True)
-    following_count = serializers.IntegerField(read_only=True)
-    is_following = serializers.BooleanField(read_only=True, default=None)
+    class Meta:
+        model=User
+        fields=["username", "first_name", "last_name", "email", "private", "verified"]
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    username = serializers.RegexField(regex=r'^[a-zA-Z]+([._-]?[a-zA-Z0-9]+)+$', min_length=3, required=True)
+    password = serializers.CharField(write_only=True, required=True)
+    password_confirmation = serializers.CharField(write_only=True, required=True)
+    image = serializers.ImageField(write_only=True, required=False)
 
     class Meta:
-        model = UserProfile
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'is_private', 'is_verified', 'image', 'followers_count', 'following_count', 'is_following'] 
-
-    def to_representation(self, instance):
-        old = super().to_representation(instance)
-        return old
-
+        model = User
+        fields=("username", "first_name", "last_name", "email", "password", "password_confirmation", "image")
+    
     def create(self, validated_data):
-        user = UserProfile.objects.create_user(**validated_data)
+        username = validated_data.get('username')
+        first_name = validated_data.get('first_name')
+        last_name = validated_data.get('last_name')
+        email = validated_data.get('email')
+        password = validated_data.get('password')
+        password_confirmation = validated_data.get('password_confirmation')
+        image = validated_data.get('image')
+
+        if username and User.objects.filter(username=username).exists():
+            raise serializers.ValidationError({"username": "Username must be unique."})
+
+        if email and User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({"email": "Email addresses must be unique."})
+
+        if password != password_confirmation:
+            raise serializers.ValidationError({"password": "The password confirmation is not valid."})
+        
+        user = User(first_name=first_name, last_name=last_name, email=email, username=username, image=image)
+
+        user.set_password(password)
+
+        user.save()
+
         return user
 
+class UserUpdateSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(write_only=True, required=False)
+    private =  serializers.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['private', 'image']
+
     def update(self, instance, validated_data):
-        instance.is_private = validated_data.get("is_private", instance.is_private)
+        instance.private = validated_data.get("private", instance.private)
         instance.image = validated_data.get("image", instance.image)
+        instance.save()
         return instance
 
-class RegisterSerializer(UserSerializer):
-        password_confirmation = serializers.CharField(min_length=8, max_length=50, write_only=True)
-
-        class Meta:
-            model = UserProfile
-            fields = ['first_name', 'last_name', 'username', 'email', 'password', 'is_private', 'is_verified', 'image', 'password_confirmation']
-
-        def validate(self, attrs):
-            if attrs['password'] != attrs['password_confirmation']:
-                raise serializers.ValidationError({'password': ('password is not confurmed.')})
-
-            attrs.pop("password_confirmation")
-
-            if UserProfile.objects.filter(username=attrs['username']).exists():
-                raise serializers.ValidationError({'username': ('username most be unique')})
-
-            if UserProfile.objects.filter(email=attrs['email']).exists():
-                raise serializers.ValidationError({'email': ('email most be unique')})
-
-            return super().validate(attrs)
-
-        def create(self, validated_data):
-            user = UserProfile.objects.create_user(**validated_data)
-            return user
-
-
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.ModelSerializer):
     username = serializers.RegexField(regex=r'^[a-zA-Z]+([._-]?[a-zA-Z0-9]+)+$', min_length=3, max_length=30)
     password = serializers.CharField(min_length=8, max_length=50, write_only=True)
 
     class Meta:
-        fields = ['username', 'password']
+        model=User
+        fields = ('username', 'password')
 
     def create(self, validated_data):
-        return authentication.authenticate(**validated_data)
+        username = validated_data.get('username')
+        password = validated_data.get('password')
 
+        user = authentication.authenticate(username=username, password=password)
 
-class UserUpdate(serializers.Serializer):
-    image = serializers.ImageField(required=False, write_only=True, allow_empty_file=False, allow_null=False)
-    is_private =  serializers.BooleanField(required=False)
-
-    class Meta:
-        model = UserProfile
-        fields = ['is_private', 'image']
-
-    def update(self, instance, validated_data):
-        instance.is_private = validated_data.get("is_private", instance.is_private)
-        instance.image = validated_data.get("image", instance.image)
-        instance.save()
-        return instance
+        if user == None:
+            raise serializers.ValidationError({"username": "Username or password is incorrect"}, 404)
+        
+        return user
