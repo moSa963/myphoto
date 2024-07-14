19051bdc1b98dc0131f2b998e71b46db06c12da9
moSa963 <mo.sa963@hotmail.com>
2024-07-14 16:14:42 +0300

    Add post test helpers

---
+++ tests/helpers/post_helpers.py
@@ -0,0 +1,30 @@
+from posts.models import Post, PostImage
+from PIL import Image
+from io import BytesIO
+
+def getMediaRoot() ->str:
+    return 'media/test'
+    
+def createImage() -> BytesIO:
+    image = Image.new('RGB', (100, 100), color = 'red')
+    image_io = BytesIO()
+    image.save(image_io, format='JPEG')
+    image_io.seek(0)
+    
+    return image_io
+    
+def addImageToPost(post: Post) -> PostImage:
+    image = PostImage(post=post)
+    image_io = createImage()
+    
+    image.image.save("test.jpg", image_io)
+    image.save()
+    
+    return image
+    
+def createPost(user, title) -> Post:
+    post = Post.objects.create(user=user, title=title)
+
+    addImageToPost(post)
+    
+    return post
