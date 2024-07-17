5b8d091bde631d409a386c95c02169f0a79d9e43
moSa963 <mo.sa963@hotmail.com>
2024-07-17 22:13:02 +0300

    Refactor FollowSerializer to reflect model changes and add create method

--- relations/serializers.py
+++ relations/serializers.py
@@ -1,14 +1,38 @@
 from rest_framework import serializers
-from api.serializers import UserSerializer
-from .models import Follow
+from users.models import User
+from users.serializers import UserSerializer
+from relations.models import Follow
 
-class FollowSerializer(serializers.Serializer):
-    id = serializers.CharField(read_only=True)
-    user = UserSerializer()
-    following = UserSerializer()
-    is_verified = serializers.BooleanField(read_only=True)
-    following_date = serializers.DateTimeField(read_only=True)
-
+class FollowSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    followed_user = UserSerializer(read_only=True)
+    
+    user_id = serializers.IntegerField(required=True, write_only=True)
+    followed_user_id = serializers.IntegerField(required=True, write_only=True)
+    
+    verified = serializers.BooleanField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    
     class Meta:
         model = Follow
-        fields = ['user', 'following', 'is_verified', 'following_date']
+        fields=("user", "followed_user", "verified", "created_at", "user_id", "followed_user_id")
+        
+    def create(self, validated_data):
+        user_id = validated_data.get("user_id")
+        followed_user_id = validated_data.get("followed_user_id")
+        
+        if user_id == followed_user_id:
+            raise serializers.ValidationError({"user": "A user could not follow himself."})
+        
+        follow,_ = Follow.objects.get_or_create(user_id=user_id, followed_user_id=followed_user_id)
+        
+        if follow.verified:
+            return follow
+    
+        followed_user = User.objects.get(pk=followed_user_id)
+        
+        if not followed_user.private:
+            follow.verified = True
+            follow.save()
+            
+        return follow
