7bfa6e602d78486cefddb5a3b60cce8948869fde
moSa963 <mo.sa963@hotmail.com>
2024-07-18 22:23:13 +0300

    Refactor relations app views to reflect model changes and for better readability

--- relations/views.py
+++ relations/views.py
@@ -1,113 +1,88 @@
-from django.db.models.expressions import F, Case, When
-from django.db.models.fields import BooleanField
-from django.db.models.query_utils import Q
-from api.models import UserProfile
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, ListAPIView
 from relations.serializers import FollowSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Follow
-from rest_framework.generics import GenericAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated
-from api.permissions import is_user_accessble
-from rest_framework.exceptions import NotAuthenticated, NotFound
+from django.http import Http404
+from users.models import User
+from django.db.models import Q
+from relations.permissions import is_user_accessible
 # Create your views here.
 
+
 class FollowView(GenericAPIView):
-    permission_classes = [IsAuthenticated]
     serializer_class = FollowSerializer
-
-    #follow a user
-    def post(self, request, **kwargs):
-        try:
-            username = kwargs.get('username', 0)
-            following = UserProfile.objects.filter(username=username).first()
-            id = str(request.user.id) + "-" + str(following.id)
-            
-            follow, u = Follow.objects.get_or_create(id=id, defaults={"user_id": request.user.id, "following_id": following.id})
-
-            #if the user that getting followed not private make the request accepted "verified"
-            if not following.is_private:
-                Follow.objects.filter(id=follow.id).update(is_verified=True)
-
-            follow = self.get_serializer(Follow.objects.get(id=follow.id))
-            return Response(data=follow.data, status=status.HTTP_200_OK)
-        except:
-            return Response({'message': 'username is wrong or not exists'}, status=status.HTTP_404_NOT_FOUND)
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, *args, **kwargs):
+        following = get_object_or_404(get_user_model(), username=kwargs["username"])
         
+        serializer =  self.get_serializer(data={
+            "user_id": request.user.id,
+            "followed_user_id": following.id,
+        })
+        
+        serializer.is_valid(raise_exception=True)
+        
+        serializer.save()
+        
+        return Response(data=serializer.data)
 
-    #unfollow a user
-    def delete(self, request, **kwargs):
-        try:
-            following_username = kwargs.get('username', 0)
-            UserProfile.objects.get(username=kwargs.get('username', 0))
-            Follow.objects.filter(user=request.user, following__username=following_username).delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except:
-            return Response({'message': 'username is wrong or does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-
-    #accept a follow request
-    def put(self, request, **kwargs):
-        id = kwargs.get("id", None)
-
-        try:                
-            follow = Follow.objects.get(id=id)
-
-            #the requested user should be the same user that getting followed
-            if not follow.following_id == request.user.id:
-                return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-            Follow.objects.filter(id=id).update(is_verified=True)
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def delete(self, request, *args, **kwargs):
+        request.user.following.filter(followed_user__username=kwargs["username"]).delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class AcceptFollowView(GenericAPIView):
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, *args, **kwargs):
+        follow = request.user.followers.filter(user__username=kwargs["username"]).first()
+        
+        if not follow:
+            raise Http404()
+        
+        follow.set_verified()
+        
+        return Response(self.get_serializer(instance=follow).data)
 
 class FollowingListView(ListAPIView):
     serializer_class = FollowSerializer
+    permission_classes=[IsAuthenticated]
     
     def get_queryset(self):
-        username = self.kwargs.get('username', None)
-        user = UserProfile.objects.get(username=username)
+        user = get_object_or_404(get_user_model(), username=self.kwargs["username"])
+        auth = self.request.user
+        
+        is_user_accessible(self.request, user)
+        
+        query = user.following
 
-        if not user or not is_user_accessble(self.request, user):
-            raise NotAuthenticated(detail={"message": "user is not accessble"})
-
-        key = self.request.GET.get("key", None)
-
-        query = Follow.objects.filter(user_id=user.id, is_verified=True )
-        if key:
-            query = query.filter(Q(following__username__icontains=key) | Q(following__first_name__icontains=key) | Q(following__last_name__icontains=key))
-
+        if user.id != auth.id:
+            query = query.filter(Q(followed_user__private=False) | (Q(followed_user__followers__user_id=auth.id) & Q(followed_user__followers__verified=True)))
+            
         return query
 
-
-
+    
 class FollowersListView(ListAPIView):
     serializer_class = FollowSerializer
-
+    permission_classes=[IsAuthenticated]
+    
     def get_queryset(self):
-        username = self.kwargs.get('username', None)
-        user = UserProfile.objects.filter(username=username).first()
+        user = get_object_or_404(get_user_model(), username=self.kwargs["username"])
+        auth = self.request.user
 
-        if not user or not is_user_accessble(self.request, user):
-            raise NotAuthenticated(detail={"message": "user is not accessble"})
-
-        unverified = self.request.GET.get("unverified", None)
-
-        if unverified and unverified == "true":
-            if user.username == self.request.user.username:
-                return Follow.objects.filter(following_id=self.request.user.id, is_verified=False).select_related()
-            else:
-                raise NotFound()
+        is_user_accessible(self.request, user)
         
-        key = self.request.GET.get("key", None)
-
-        query = Follow.objects.filter(following_id=user.id, is_verified=True)
-        if key:
-            query = query.filter(Q(following_id=user.id), Q(is_verified=True) and (Q(user__username__icontains=key) | Q(user__first_name__icontains=key) | Q(user__last_name__icontains=key)))
-
+        query = user.followers
+        
+        if user.id != auth.id:
+            query = query.filter(Q(user__private=True) | (Q(user__followers__user_id=auth.id) & Q(user__followers__verified=True)))
+        
         return query
-
