6957beb56bed16a4f3a9605287344c80fb2d2f63
moSa963 <mo.sa963@hotmail.com>
2024-07-19 22:37:48 +0300

    Add 'is_post_accessible' function

--- posts/permissions.py
+++ posts/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions
+from relations.permissions import is_user_accessible
+from django.core.exceptions import PermissionDenied
+
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -16,3 +19,13 @@ class IsOwner(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+    
+
+def is_post_accessible(request, post):
+    if post.user_id == request.user.id:
+        return
+    
+    if not post.private and is_user_accessible(request, post.user):
+        return
+    
+    raise PermissionDenied()
--- relations/permissions.py
+++ relations/permissions.py
@@ -8,4 +8,4 @@ def is_user_accessible(request, user):
         if not Follow.objects.filter(user_id=request.user.id, followed_user__id=user.id, verified=True).exists():
             raise PermissionDenied("You cannot access this user information.")
 
-    
+    return True
