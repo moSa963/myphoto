14b728948f07c92f6fecc2048dff8bdce0e12813
moSa963 <mo.sa963@hotmail.com>
2024-07-19 22:36:08 +0300

    Remove like app

--- like/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CommentLikeView, LikeListView, LikeView, PostsLikedView
-
-urlpatterns = [
-    path('post/<int:id>', LikeView.as_view()),
-    path('comment/<int:id>', CommentLikeView.as_view()),
-    path('list', LikeListView.as_view()),
-    path('post/list', PostsLikedView.as_view()),
-]
--- myphoto/settings.py
+++ myphoto/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'posts',
-    'follow',
-    'like',
+    'relations',
     'comment',
     'history',
 ]
