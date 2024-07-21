3c2093132b379133d79689d81e34a9b23863862b
moSa963 <mo.sa963@hotmail.com>
2024-07-21 13:32:56 +0300

    Add CommentLikeView an refactor comments views to reflect the model changes

--- comments/views.py
+++ comments/views.py
@@ -1,46 +1,95 @@
-from django.db.models.aggregates import Count
-from django.db.models.query_utils import Q
-from comment.models import Comment
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, ListAPIView
+from comments.models import PostComment, CommentLike
+from comments.serializers import CommentSerializer, CommentLikeSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from posts.models import Post
 from rest_framework import status
 from rest_framework.response import Response
-from .serializers import CommentSerializer
-from rest_framework.generics import GenericAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated
-
+from comments.permissions import is_owner
+from posts.permissions import is_post_accessible
+from django.db.models import Count, Q, Exists, OuterRef
 # Create your views here.
 
-class CommentView(GenericAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = CommentSerializer
 
+class CommentCreateView(GenericAPIView):
+    serializer_class=CommentSerializer
+    permission_classes=[IsAuthenticated]
+    
     def post(self, request, **kwargs):
-        comment = self.get_serializer(data=request.data)
+        post = get_object_or_404(Post, id=kwargs["post_id"])
+        
+        is_post_accessible(request, post)
+        
+        comment = self.get_serializer(data={
+            "user_id": request.user.id,
+            "post_id": post.id,
+            "content": request.data.get("content")
+        })
+        
         comment.is_valid(raise_exception=True)
         comment.save()
-        return Response(status=status.HTTP_201_CREATED)
+        
+        return Response(data=comment.data, status=status.HTTP_201_CREATED)
+        
 
+class CommentDeleteView(GenericAPIView):
+    serializer_class=CommentSerializer
+    permission_classes=[IsAuthenticated]
+    
     def delete(self, request, **kwargs):
-        try:
-            comment = Comment.objects.get(id=kwargs["id"])
-            if comment and comment.user_id == request.user.id:
-                comment.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-class CommentList(ListAPIView):
-    serializer_class = CommentSerializer
-
+        post = get_object_or_404(Post, id=kwargs["post_id"])
+        comment = get_object_or_404(PostComment, post_id=post.id, id=kwargs["id"])
+        
+        is_owner(request, comment)
+        
+        comment.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class CommentListView(ListAPIView):
+    serializer_class=CommentSerializer
+    permission_classes=[IsAuthenticated]
+    
     def get_queryset(self):
-        query = Comment.objects.filter(post_id=self.kwargs.get("post_id")
-                    ).annotate(like=Count("likes"), user_liked=Count("likes", filter=Q(likes__user__id=1)))
+        post = get_object_or_404(Post, id=self.kwargs["post_id"])
+        
+        is_post_accessible(self.request, post)
+        
+        q = post.comments.annotate(
+            likes_count=Count("likes"), liked=Exists(CommentLike.objects.filter(comment_id=OuterRef("pk"), user_id=self.request.user.id))
+        )
+        
+        return q
+    
+    
+class CommentLikeView(GenericAPIView):
+    serializer_class=CommentLikeSerializer
+    permission_classes=[IsAuthenticated]
+    
+    def post(self, request, **kwargs):
+        post = get_object_or_404(Post, id=kwargs['post_id'])
+        is_post_accessible(request, post)
+        
+        comment = get_object_or_404(post.comments, id=kwargs['id'])
+        
+        serializer = self.get_serializer(data={
+            "user_id": request.user.id,
+            "comment_id": comment.id,
+        })
 
-        sort = self.request.GET.get("sort", "")
-        if sort == "new":
-            query = query.order_by('-date')
-        elif sort == "old":
-            query = query.order_by('date')
-        elif sort == "likes":
-            query = query.order_by("-like")
-        return query
+        serializer.is_valid(raise_exception=True)        
+        serializer.save()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def delete(self, request, **kwargs):
+        post = get_object_or_404(Post, id=kwargs['id'])
+        
+        like = request.user.comments_likes.filter(comment__post_id=post.id).first()
+
+        if like:
+            like.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
