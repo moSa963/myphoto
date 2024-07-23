21c4720a7b31db3288d403ab2a9c6025f101d196
moSa963 <mo.sa963@hotmail.com>
2024-07-23 21:32:05 +0300

    Fix related_likes returns hidden posts and rename the method to 'for_user'

--- posts/manager.py
+++ posts/manager.py
@@ -4,7 +4,8 @@ from posts.models import User
 from typing import Self
 
 class PostManager(models.Manager):
-    def posts(self, user: User, order_by: str="new") -> Self:
+    
+    def for_user(self, user: User, order_by: str="new") -> Self:
         q = self.select_related("user").filter(
             Q(user_id=user.id) |
                 (Q(private=False) &
@@ -23,7 +24,7 @@ class PostManager(models.Manager):
 
     def related_posts(self, user: User, order_by: str="new") -> Self:
         q = self.select_related("user").filter(
-            Q(user_id=user.id) | (Q(user__followers__user_id=user.id) & Q(user__followers__verified=True))
+            Q(user_id=user.id) | (Q(private=False) & Q(user__followers__user_id=user.id) & Q(user__followers__verified=True))
                 ).annotate(
                     likes_count=Count("likes", distinct=True),
                     comments_count=Count("comments"),
@@ -45,11 +46,12 @@ class PostManager(models.Manager):
 
 class PostLikeManager(models.Manager):
     
-    def related_likes(self, user: User):
-        q = self.select_related("user").filter(Q(user_id=user.id) | Q(user__private=False) |
-                        (Q(user__followers__user_id=user.id) & Q(user__followers__verified=True))
-                    )
-
+    def for_user(self, user: User):
+        q = self.select_related("user").filter(
+            (Q(post__user_id=user.id) | (Q(post__private=False) & (Q(post__user__private=False) | (Q(post__user__followers__user_id=user.id) & Q(post__user__followers__verified=True)))))
+            & (Q(user_id=user.id) | Q(user__private=False) | (Q(user__followers__user_id=user.id) & Q(user__followers__verified=True)))
+            )
+                
         return q
     
     
