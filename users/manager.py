10e8d3ec6fd2fa3daf3b868e434d2087f6c13da4
moSa963 <mo.sa963@hotmail.com>
2024-08-27 19:02:28 +0300

    Fix users search results are duplicated

--- users/manager.py
+++ users/manager.py
@@ -1,12 +1,23 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Case, When, Count, F, BooleanField
+from django.db.models import Case, When, Count, OuterRef, BooleanField, Value, Subquery
 from django.contrib.auth.models import UserManager
 
 class UserManager(UserManager):
     def for_user(self, user, key: str=""):
-        query = get_user_model().objects.filter(username__icontains=key).annotate(
-            followers_count=Count("followers"), 
-            following_status=Case(When(followers__user_id=user.id, then=F("followers__verified")), output_field=BooleanField(null=True))
-        ).order_by("followers_count")
+        following_status_subquery = get_user_model().objects.filter(
+            followers__user_id=user.id,
+            pk=OuterRef('pk')
+        ).values('followers__verified')[:1]
+
+        query = get_user_model().objects.filter(
+            username__icontains=key
+        ).annotate(
+            followers_count=Count("followers", distinct=True), 
+            following_status=Subquery(following_status_subquery)
+        ).order_by("-followers_count")
         
         return query
+    
+    
+    
+    
